496def54307b9afe717ea13de763e71fa7210a68
brianocm9 <brianocm9@gmail.com>
2024-09-04 16:04:58 +0000

    gh pages and title html

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://BrianC9.github.io/task-manager",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
